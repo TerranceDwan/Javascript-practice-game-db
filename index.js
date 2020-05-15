e51fa1b8214180b60fda1010ff6ef8598e252541
TerranceDwan <terrance4869@gmail.com>
2020-05-15 13:55:30 -0500

    Added Access-Control-Allow-Origin header

--- index.js
+++ index.js
@@ -11,7 +11,11 @@ connectDB()
 app.use(express.json())
 app.use(router)
 app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', 'http://localhost:3000/')
+  res.header('Access-Control-Allow-Origin', '*')
+  res.header(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-Type, Accept'
+  )
   next()
 })
 

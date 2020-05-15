bc1fbef309b8806f13d7991f736188a79ce380d9
TerranceDwan <terrance4869@gmail.com>
2020-05-15 12:30:02 -0500

    Changing URI to URL

--- db/mongoose.js
+++ db/mongoose.js
@@ -1,6 +1,6 @@
 var mongoose = require('mongoose')
 
-const URI = process.env.MONGODB_URI
+const URI = process.env.MONGODB_URL
 const connectDB = async () => {
   await mongoose.connect(URI, {
     useNewUrlParser: true,
